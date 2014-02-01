d309953b74ada849fea95bd1d60b7a5e0de4296f
Vaibhav Bhandari <vaibhavb@vitraag.com>
2014-02-01 01:17:30 -0800

    Adding packs

---
+++ apps/ccda-reDirect/launch.js
@@ -0,0 +1,156 @@
+var argv = require("optimist").argv;
+var request = require("request");
+var crypto = require('crypto');
+var async = require('async');
+var template = require('url-template');
+var AdmZip = require('adm-zip');
+var path = require('path');
+
+var sourceDir = argv.source_dir || process.env["SOURCE_DIR"];
+var progressDir = argv.progress_dir || process.env["PROGRESS_DIR"];
+var httpPostUrlTemplate = argv.ccda_post_url || process.env["CCDA_POST_URL"];
+var httpPostUrl = template.parse(httpPostUrlTemplate);
+
+console.log(argv);
+Watcher = require("./watcher");
+
+w = new Watcher({
+  sourceDir: sourceDir,
+  progressDir: progressDir
+});
+
+w.on("incoming", function(filename, message){
+  console.log("incoming message", filename);
+
+  if (message.attachments){
+
+var argv = require("optimist").argv;
+var request = require("request");
+var crypto = require('crypto');
+var async = require('async');
+var template = require('url-template');
+var AdmZip = require('adm-zip');
+var path = require('path');
+
+var sourceDir = argv.source_dir || process.env["SOURCE_DIR"];
+var progressDir = argv.progress_dir || process.env["PROGRESS_DIR"];
+var httpPostUrlTemplate = argv.ccda_post_url || process.env["CCDA_POST_URL"];
+var httpPostUrl = template.parse(httpPostUrlTemplate);
+
+console.log(argv);
+Watcher = require("./watcher");
+
+w = new Watcher({
+  sourceDir: sourceDir,
+  progressDir: progressDir
+});
+
+w.on("incoming", function(filename, message){
+  console.log("incoming message", filename);
+
+  var posts = []
+
+  message.to.forEach(function(to)) {
+   var url = "http://localhost:8080/api/messages&user={{to}}".expand(
+      {
+        to: message.to[0].address
+      }
+    );
+   body = {
+      to: message.to[0].address,
+      from: message.from[0].address,
+      subject: message.subject,
+      body: message.body
+    };
+
+   posts.push({url: url, body: body});
+  }
+
+  //All Messages out
+  console.log("Writing all messages: ", posts.length, " posts");
+  async.each(posts, function(p, callback) {
+    console.log("POSTing to ", p.url);
+    request(
+      { method: 'POST',
+        url: p.url,
+        body: p.body,
+        headers: {"Content-type": "text/xml"}
+      }, function (error, response, body) {
+        if (response && response.statusCode === 200){
+          console.log("POSTed ", filename, response.statusCode);
+          return callback();
+        }
+        return callback(error||response.statusCode);
+      });
+    }, function(err){
+    if (!err) {
+      w.markComplete(filename);
+      console.log("Marked progress for ", filename);
+    } else {
+      console.log("Error POSTing for", filename, err);
+    }
+  });
+
+
+  if (message.attachments){
+
+    var posts = [];
+
+    var ccdas = message.attachments.filter(function(a){
+      return a.fileName && a.fileName.slice("-3").toLowerCase() === "xml";
+    });
+
+    message.attachments.filter(function(a){
+      return a.fileName && a.fileName.slice("-3").toLowerCase() === "zip";
+    }).forEach(function(a){
+      new AdmZip(a.content).getEntries().forEach(function(e){
+        var ext = path.extname(e.entryName).toLowerCase();
+        if (ext !== '.xml') {
+          return;
+        }
+        if (path.basename(e.entryName).toLowerCase() === 'metadata.xml') {
+          return;
+        }
+        ccdas.push({ content: e.getData() });
+      });
+    });
+
+    message.to.forEach(function(to){
+
+      var url = httpPostUrl.expand({
+        to: message.to[0].address,
+        from: message.from[0].address
+      });
+
+      ccdas.forEach(function(ccda){
+        posts.push({url: url, ccda:  ccda});
+      });
+
+    });
+
+    console.log("C-CDA Attachments: ", ccdas.length, posts.length, " posts");
+    async.each(posts, function(p, callback) {
+      console.log("POSTing to ", p.url);
+      request(
+        { method: 'POST',
+          url: p.url,
+          body: p.ccda.content,
+          headers: {"Content-type": "text/xml"}
+        }, function (error, response, body) {
+          if (response && response.statusCode === 200){
+            console.log("POSTed ", filename, response.statusCode);
+            return callback();
+          }
+          return callback(error||response.statusCode);
+        });
+    }, function(err){
+      if (!err) {
+        w.markComplete(filename);
+        console.log("Marked progress for ", filename);
+      } else {
+        console.log("Error POSTing for", filename, err);
+      }
+    });
+  }
+});
+                                                      
---
+++ carepack/app.js
@@ -0,0 +1,52 @@
+
+
+var express = require('express');
+var db = require('mongojs').connect('127.0.0.1:27017/carepack');
+
+function index(req, res, next){
+  res.send("API Schema in RAML - just kidding!")
+}
+
+function getMessages(req, res, next){
+
+  messages = db.collection('messages');
+  messages.find({"to": req.query.user}, function(err, msg) {
+    console.log("Get Message for: " + req.query.user)
+    res.send(msg);
+  });
+}
+
+function postMessages(req, res)
+{
+  var message;
+  console.log('POST message:');
+  console.log(req.body);
+  message = req.body;
+  messages = db.collection('messages');
+  d = messages.insert(message);
+  res.send(d)
+}
+
+function testRespond(req, res, next) {
+  res.send('hello ' + req.params.name);
+}
+
+var apiserver = express();
+// config 
+apiserver.use(express.json());
+apiserver.use(express.urlencoded());
+apiserver.use(apiserver.router)
+
+/* APIs */
+apiserver.get('/api', index)
+
+apiserver.post('/api/messages', postMessages);
+apiserver.get('/api/messages', getMessages);
+
+apiserver.get('/hello/:name', testRespond);
+apiserver.head('/hello/:name', testRespond);
+
+
+
+/* Start Server */
+apiserver.listen(8080);
